89563ea1987b14403945543af5808b3f1580dfe7
matrixfox <matrixfox@gmail.com>
2014-11-01 12:43:44 -0700

    Fixed Nodemailer Error
    Fixed [Error: Unsupported configuration, downgrade Nodemailer to v0.7.1
    or see the migration guide
    https://github.com/andris9/Nodemailer#migration-guide]

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var request = require('request');
-var mongoose = require('mongoose');
 var nodemailer = require("nodemailer");
 
 // create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("SMTP",{
+var smtpTransport = nodemailer.createTransport({
     service: "Gmail",
     auth: {
         user: "********************",
@@ -17,24 +16,6 @@ var header = {
         'User-Agent': 'nbtc'
     }
 };
-
-mongoose.connect('mongodb://localhost/bitcoin');
-
-// Doctype schema
-var Schema = mongoose.Schema;
-var schema = new Schema({
-    high: String,
-    last: String,
-    timestamp: String,
-    bid: String,
-    volume: String,
-    low: String,
-    ask: String
-}, { versionKey: false});
-schema.set('toObject', { getters: true });
-
-// Grabbing the schema and connecting to mongodb database
-var Bitcoin = mongoose.model('bitstamp', schema);
 
 //Set Alerts
 // if price drops lower than =
@@ -57,69 +38,53 @@ function nodeify() {
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-
-
-        // dumping json var
-        var chart = new Bitcoin(info);
-            chart.save(function (err) {
-              if (err) console.warn(err.message);
-              // mongoose.connection.close();
-            });
+        var json = JSON.parse(body);
 
 
         // Lower Alarm
-        if (info.last < lowerAlarm && b == true) {
+        if (json.last < lowerAlarm && b == true) {
             b = false;
             // setup e-mail data with unicode symbols
             var mailOptions = {
                 from: "Matrixfox <matrixfox@gmail.com>", // sender address
                 to: "matrixfox@gmail.com", // list of receivers
                 subject: "Bitcoin Alert", // Subject line
-                // text: "BUY " + info.last + " is less than " + lowerAlarm, // plaintext body
-                html: '<p>BUY OUT - Last: <b>' + info.last + " </b>is under your Alarm Price: <b> " + lowerAlarm + '</b></p>' // html body
+                html: '<p>BUY OUT - Last: <b>' + json.last + " </b>is under your Alarm Price: <b> " + lowerAlarm + '</b></p>' // html body
             }
 
 
             // send mail with defined transport object
-            smtpTransport.sendMail(mailOptions, function(error, response){
+            smtpTransport.sendMail(mailOptions, function(error, info){
                 if(error){
                     console.log(error);
+                }else{
+                    console.log('Message sent: ' + info.response);
+                    process.kill();
                 }
-                // else{
-                //     console.log("Message sent: " + response.message)
-                // }
-
-                // if you don't want to use this transport object anymore, uncomment following line
-                smtpTransport.close(); // shut down the connection pool, no more messages
             });
         }// end lowerAlarm
 
 
         // Over Alarm
-        if (info.last > overAlarm && s == true) {
+        if (json.last > overAlarm && s == true) {
             s = false;
             // setup e-mail data with unicode symbols
             var mailOptions = {
                 from: "Matrixfox <matrixfox@gmail.com>", // sender address
                 to: "matrixfox@gmail.com", // list of receivers
                 subject: "Bitcoin Alerts", // Subject line
-                // text: "SELL " + info.last + " is over than" + overAlarm, // plaintext body
-                html: '<p>SELL OFF - Last: <b>' + info.last + " </b>is over your Alarm Price: <b> " + overAlarm + '</b></p>' // html body
+                html: '<p>SELL OFF - Last: <b>' + json.last + " </b>is over your Alarm Price: <b> " + overAlarm + '</b></p>' // html body
             }
 
 
             // send mail with defined transport object
-            smtpTransport.sendMail(mailOptions, function(error, response){
+            smtpTransport.sendMail(mailOptions, function(error, info){
                 if(error){
                     console.log(error);
+                }else{
+                    console.log('Message sent: ' + info.response);
+                    process.kill();
                 }
-                // else{
-                //     console.log("Message sent: " + response.message);
-                // }
-
-                // if you don't want to use this transport object anymore, uncomment following line
-                smtpTransport.close(); // shut down the connection pool, no more messages
             });
         }// end overAlarm
 
